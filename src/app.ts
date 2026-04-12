739c53fb11a670763e861d34b8c5f3cc3841930f
Fiona Veseli <74466339+fionaveseli@users.noreply.github.com>
2026-04-12 14:49:57 +0200

    setup typescript

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "SASA backend is running",
+  });
+});
+
+export default app;
